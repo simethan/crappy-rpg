34a24d70cc09ebc776897de84ec84443ee80c50c
eaten <76576856+eatenliao@users.noreply.github.com>
2021-04-03 00:12:09 +0800

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,9 @@
-import pygame, sys, os, time
+import pygame, sys, os, time, random
 
 clock = pygame.time.Clock()
 
 from pygame.locals import *
+pygame.mixer.pre_init(44100, -16, 2, 512)
 pygame.init()
 
 pygame.display.set_caption('mak gam')
@@ -11,23 +12,26 @@ WINDOW_SIZE = (600,400)
 
 screen = pygame.display.set_mode(WINDOW_SIZE,0,32) 
 
-display = pygame.Surface((300,200)) 
-moving_right = False
-moving_left = False
-vertical_momentum = 0
-air_timer = 0
 
-true_scroll = [0,0]
+display = pygame.Surface((300,200)) ; moving_right = False ; moving_left = False ; vertical_momentum = 0 ; air_timer = 0 ; true_scroll = [0,0] ; CHUNK_SIZE = 15
 
-def load_map(path):
-    f = open(path + '.txt','r')
-    data = f.read()
-    f.close()
-    data = data.split('\n')
-    game_map = []
-    for row in data:
-        game_map.append(list(row))
-    return game_map
+def generate_chunk(x,y):
+    chunk_data = []
+    for y_pos in range(CHUNK_SIZE):
+        for x_pos in range(CHUNK_SIZE):
+            target_x = x * CHUNK_SIZE + x_pos
+            target_y = y * CHUNK_SIZE + y_pos
+            tile_type = 0 
+            if target_y > 10:
+                tile_type = 2
+            elif target_y == 10:
+                tile_type = 1
+            elif target_y == 9:
+                if random.randint(1,5) == 1:
+                    tile_type = 3 
+            if tile_type != 0:
+                chunk_data.append([[target_x,target_y],tile_type])
+    return chunk_data
 
 global animation_frames
 animation_frames = {}
@@ -60,14 +64,36 @@ animation_database = {}
 animation_database['run'] = load_animation('C:/Users/user/Documents/PyGameFolder/pewpewanimation/run',[7,7])
 animation_database['idle'] = load_animation('C:/Users/user/Documents/PyGameFolder/pewpewanimation/idle',[7,7,40])
 
-game_map = load_map('C:/Users/user/Documents/PyGameFolder/map')
+game_map = {}
 
 grass_img = pygame.image.load('C:/Users/user/Documents/PyGameFolder/grass.png')
 dirt_img = pygame.image.load('C:/Users/user/Documents/PyGameFolder/dirt.png')
+plant_img = pygame.image.load('C:/Users/user/Documents/PyGameFolder/plant.png').convert()
+plant_img.set_colorkey((255,255,255))
+rock_img = pygame.image.load('C:/Users/user/Documents/PyGameFolder/rock.png')
+cobble_img = pygame.image.load('C:/Users/user/Documents/PyGameFolder/cobble.png')
+
+tile_index = {1:grass_img,
+              2:dirt_img,
+              3:plant_img,
+              4:rock_img,
+              5:cobble_img
+              }
+
+
+jump_sound = pygame.mixer.Sound('C:/Users/user/Documents/PyGameFolder/jump.wav')
+grass_sounds = [pygame.mixer.Sound('C:/Users/user/Documents/PyGameFolder/grass_0.wav'),pygame.mixer.Sound('C:/Users/user/Documents/PyGameFolder/grass_1.wav')]
+grass_sounds[0].set_volume(0.2)
+grass_sounds[1].set_volume(0.2)
+
+pygame.mixer.music.load('C:/Users/user/Documents/PyGameFolder/music.wav')
+pygame.mixer.music.play(-1)
 
 player_action = 'idle'
 player_frame = 0
 player_flip = False
+
+grass_sound_timer = 0
 
 player_rect = pygame.Rect(100,100,23,23)
 
@@ -105,12 +131,14 @@ def move(rect,movement,tiles):
 while True: 
     display.fill((146,244,255)) 
 
+    if grass_sound_timer > 0:
+        grass_sound_timer -= 1
+
     true_scroll[0] += (player_rect.x-true_scroll[0]-152)/20
     true_scroll[1] += (player_rect.y-true_scroll[1]-106)/20
     scroll = true_scroll.copy()
     scroll[0] = int(scroll[0])
     scroll[1] = int(scroll[1])
-
     pygame.draw.rect(display,(7,80,75),pygame.Rect(0,120,300,80))
     for background_object in background_objects:
         obj_rect = pygame.Rect(background_object[1][0]-scroll[0]*background_object[0],background_object[1][1]-scroll[1]*background_object[0],background_object[1][2],background_object[1][3])
@@ -120,18 +148,18 @@ while True:
             pygame.draw.rect(display,(9,91,85),obj_rect)
 
     tile_rects = []
-    y = 0
-    for layer in game_map:
-        x = 0
-        for tile in layer:
-            if tile == '1':
-                display.blit(dirt_img,(x*16-scroll[0],y*16-scroll[1]))
-            if tile == '2':
-                display.blit(grass_img,(x*16-scroll[0],y*16-scroll[1]))
-            if tile != '0':
-                tile_rects.append(pygame.Rect(x*16,y*16,16,16))
-            x += 1
-        y += 1
+    for y in range(3):
+        for x in range(4):
+            target_x = x - 1 + int(round(scroll[0]/(CHUNK_SIZE*16)))
+            target_y = y - 1 + int(round(scroll[1]/(CHUNK_SIZE*16)))
+            target_chunk = str(target_x) + ';' + str(target_y)
+            if target_chunk not in game_map:
+                game_map[target_chunk] = generate_chunk(target_x,target_y)
+            for tile in game_map[target_chunk]:
+                display.blit(tile_index[tile[1]],(tile[0][0]*16-scroll[0],tile[0][1]*16-scroll[1]))
+                if tile[1] in [1,2]:
+                    tile_rects.append(pygame.Rect(tile[0][0]*16,tile[0][1]*16,16,16))    
+
 
     player_movement = [0,0]
     if moving_right == True:
@@ -157,6 +185,10 @@ while True:
     if collisions['bottom'] == True:
         air_timer = 0
         vertical_momentum = 0
+        if player_movement[0] != 0:
+            if grass_sound_timer == 0:
+                grass_sound_timer = 30
+                random.choice(grass_sounds).play()
     else:
         air_timer += 1
 
@@ -173,17 +205,20 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == KEYDOWN:
-            if event.key == K_RIGHT:
+            if event.key == K_q:
+                pygame.mixer.music.fadeout(1000)
+            if event.key == K_RIGHT or event.key == K_d:
                 moving_right = True
-            if event.key == K_LEFT:
+            if event.key == K_LEFT or event.key == K_a:
                 moving_left = True
-            if event.key == K_UP:
+            if event.key == K_UP or event.key == K_SPACE or event.key == K_w:
                 if air_timer < 6:
+                    jump_sound.play()
                     vertical_momentum = -5
         if event.type == KEYUP:
-            if event.key == K_RIGHT:
+            if event.key == K_RIGHT or event.key == K_d:
                 moving_right = False
-            if event.key == K_LEFT:
+            if event.key == K_LEFT or event.key == K_a:
                 moving_left = False
         
     screen.blit(pygame.transform.scale(display,WINDOW_SIZE),(0,0))
